4b7ffd9596af797e3d45ab2ed03259d17558d023
lutfiya ainurrahman <lutfiyapr.creative@gmail.com>
2025-04-11 00:52:59 +0700

    feat: create worktools table

---
+++ app/Http/Controllers/WorktoolsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Worktools;
+use Illuminate\Http\Request;
+
+class WorktoolsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Worktools $worktools)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Worktools $worktools)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Worktools $worktools)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Worktools $worktools)
+    {
+        //
+    }
+}
