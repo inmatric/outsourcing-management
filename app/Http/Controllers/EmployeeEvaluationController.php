c26266e9ddcfe5f4071727abfb065d15bc9b01be
lutfiya ainurrahman <lutfiyapr.creative@gmail.com>
2025-04-11 01:09:40 +0700

    feat: create employee_evaluation table

---
+++ app/Http/Controllers/EmployeeEvaluationController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EmployeeEvaluation;
+use Illuminate\Http\Request;
+
+class EmployeeEvaluationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EmployeeEvaluation $employeeEvaluation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(EmployeeEvaluation $employeeEvaluation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, EmployeeEvaluation $employeeEvaluation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EmployeeEvaluation $employeeEvaluation)
+    {
+        //
+    }
+}
