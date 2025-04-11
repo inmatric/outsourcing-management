bf1da5a7cd00d36fda3e18c941ad4a00cf659bb6
Yanuarprayoga9 <yanuarprayoga.stu@pnc.ac.id>
2025-04-11 11:47:58 +0700

    fix: user login

--- resources/views/components/layouts/main-layout.blade.php
+++ resources/views/components/layouts/main-layout.blade.php
@@ -63,7 +63,7 @@
                                         role="menuitem">Dashboard</a>
                                 </li>
                                 <li>
-                                    <a href="{{ url('/profile/' . $user->id . '/edit') }}"
+                                    <a href="{{ url('/profile/' . Auth::user()->id . '/edit') }}">
                                         class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white">
                                         {{-- Icon edit --}}
                                         Update
