ef668770194658ba8e3727a8d32bf1c7ffda7efc
purboyo <brotoumbaranp@gmail.com>
2025-05-31 21:43:22 +0700

    "feat: update sidebar"

--- app/Models/LocationType.php
+++ app/Models/LocationType.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class LocationType extends Model
 {
     // Nama tabel
-    protected $table = 'location_type';
+    protected $table = 'location_types';
 
     // Kolom yang bisa diisi
     protected $fillable = [
