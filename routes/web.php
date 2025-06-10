4ba8a306ff8287416e45f6d8414bea6c7c785f24
purboyo <brotoumbaranp@gmail.com>
2025-06-10 15:55:23 +0700

    feat: tlll

--- routes/web.php
+++ routes/web.php
@@ -5,21 +5,20 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\LocationController;
 use App\Http\Controllers\LocationTypeController;
 use App\Http\Controllers\AttendanceController;
-
 use App\Http\Controllers\EmployeeContractController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProcessingWDController;
 use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\FundController;
 use App\Http\Controllers\ComplaintController;
 use App\Http\Controllers\WorkToolsController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CooperationController;
+use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\ItemFoundController;
 use App\Http\Controllers\LocationDivisionController;
 use App\Http\Controllers\LostItemController;
+use App\Http\Controllers\WorkReportController;
 use App\Http\Middleware\AuthMiddleware;
 use App\Http\Middleware\IsAdmin;
 use App\Http\Middleware\IsHrd;
@@ -39,10 +38,6 @@ Route::redirect('/worktools', '/test-worktools');
 
 // Public location routes
 Route::get('/test', [ProductController::class, 'test']);
-Route::get('/location/pdf', [LocationController::class, 'downloadPDF'])->name('location.pdf');
-Route::resource('location', LocationController::class);
-Route::resource('location-type', LocationTypeController::class);
-
 
 Route::prefix('v1')->group(function () {
     Route::prefix('products')->controller(ProductController::class)->group(function () {
@@ -56,80 +51,111 @@ Route::prefix('v1')->group(function () {
 
 // Protected routes (only accessible if logged in)
 Route::middleware([AuthMiddleware::class])->group(function () {
-    
+
     // Dashboard
     Route::get('/dashboard', [LoginController::class, 'dashboard']);
-    
+
     // Profile routes
     Route::get('/profile', fn() => view('profile'))->name('profile.edit');
     Route::get('/profile/{id}/edit', [ProfileController::class, 'show'])->name('profileform');
-    
+
     // Logout
     Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-    Route::resource('worktools', WorkToolsController::class)->except(['create', 'store']);
+    // 🛡 Admin only routes
+    Route::middleware([IsAdmin::class])->group(function () {
+        // User management
+        Route::prefix('users')->controller(UserController::class)->name('users.')->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::get('/create', 'create')->name('create');
+            Route::post('/', 'store')->name('store');
+            Route::get('/{id}/edit', 'edit')->name('edit');
+            Route::put('/{id}', 'update')->name('update');
+            Route::delete('/{id}', 'destroy')->name('destroy');
+        });
 
-    Route::prefix('location-division')->controller(LocationDivisionController::class)->name('location-division.')->group(function () {
-        Route::get('/', 'index')->name('index');
-        Route::get('/create', 'create')->name('create');
-        Route::post('/', 'store')->name('store');
-        Route::get('/{id}/edit', 'edit')->name('edit');
-        Route::put('/{id}', 'update')->name('update');
-        Route::delete('/{id}', 'destroy')->name('destroy');
+        // Admin resources
+        Route::resource('cooperations', CooperationController::class);
+        Route::resource('funds', FundController::class);
     });
 
 
-    Route::prefix('employee-contract')->controller(EmployeeContractController::class)->name('employee-contract.')->group(function () {
-        Route::get('/', 'index')->name('index');
-        Route::get('/create', 'create')->name('create');
-        Route::post('/', 'store')->name('store');
-        Route::get('/edit', 'edit')->name('edit');
-        Route::put('/{id}', 'update')->name('update');
-        Route::delete('/{id}', 'destroy')->name('destroy');
+
+
+    // 🛡 HRD only routes
+    Route::middleware([IsHrd::class])->group(function () {
+        // Worktools (except create and store which are public)
+        Route::get('/worktools/{id}/edit', [WorkToolsController::class, 'edit'])->name('worktools.edit');
+        Route::put('/worktools/{id}', [WorkToolsController::class, 'update'])->name('worktools.update');
+        Route::delete('/worktools/{id}', [WorkToolsController::class, 'destroy'])->name('worktools.destroy');
+
+        Route::get('/location/pdf', [LocationController::class, 'downloadPDF'])->name('location.pdf');
+        Route::resource('location', LocationController::class);
+        Route::resource('location-type', LocationTypeController::class);
+
+        // API routes
+        // Location Division
+        Route::prefix('location-division')->controller(LocationDivisionController::class)->name('location-division.')->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::get('/create', 'create')->name('create');
+            Route::post('/', 'store')->name('store');
+            Route::get('/{id}/edit', 'edit')->name('edit');
+            Route::put('/{id}', 'update')->name('update');
+            Route::delete('/{id}', 'destroy')->name('destroy');
+            Route::get('/petugas', 'indexPetugas')->name('index-petugas');
+            Route::put('/update-status/{id}', 'updateStatus')->name('update-status');
+        });
+
+        // Employee Contract
+        Route::prefix('employee-contract')->controller(EmployeeContractController::class)->name('employee-contract.')->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::get('/create', 'create')->name('create');
+            Route::post('/', 'store')->name('store');
+            Route::get('/edit', 'edit')->name('edit');
+            Route::put('/{id}', 'update')->name('update');
+            Route::delete('/{id}', 'destroy')->name('destroy');
+        });
+
+        // Processing WD
+        Route::get('/processing_wd', [ProcessingWDController::class, 'index']);
+        Route::get('/processing_wd/create', [ProcessingWDController::class, 'create'])->name('processing_wd.create');
     });
-
-    Route::get('/processing_wd', [ProcessingWDController::class, 'index']);
-    Route::get('/processing_wd/create', [ProcessingWDController::class, 'create'])->name('processing_wd.create');
-});
-
-
-    Route::resource('cooperations', CooperationController::class);
-    Route::resource('funds', FundController::class);
-
-
-    Route::prefix('employee-contract')->controller(EmployeeContractController::class)->group(function () {
-        Route::get('/', 'index');
-        Route::get('/create', 'create');
-        Route::post('/', 'store');
-        Route::get('/edit', 'edit');
-        Route::put('/{id}', 'update');
-        Route::delete('/{id}', 'destroy');
-    });
-
-    Route::prefix('location-division')->controller(LocationDivisionController::class)->name('location-division.')->group(function () {
-        Route::get('/', 'index')->name('index');
-        Route::get('/create', 'create')->name('create');
-        Route::post('/', 'store')->name('store');
-        Route::get('/{id}/edit', 'edit')->name('edit');
-        Route::put('/{id}', 'update')->name('update');
-        Route::delete('/{id}', 'destroy')->name('destroy');
-        Route::get('/petugas', 'indexPetugas')->name('index-petugas');
-        Route::put('/update-status/{id}', 'updateStatus')->name('update-status'); 
-    });
-
-});
-
-
-Route::get('/attendances', [AttendanceController::class, 'index'])->name('attendances.index');
-Route::get('/attendances/create', [AttendanceController::class, 'create'])->name('attendances.create');
-Route::get('/attendances/creates', [AttendanceController::class, 'creates'])->name('attendances.creates');
-Route::delete('/attendances/{id}', [AttendanceController::class, 'destroy'])->name('attendances.destroy');
-
-Route::post('/attendances', [AttendanceController::class, 'store'])->name('attendances.store'); // This is the store route
-Route::get('/employees-attendances', [AttendanceController::class, 'indexs'])->name('employees_attendances.index');
-Route::patch('/attendances/{id}/status', [AttendanceController::class, 'updateStatus'])->name('attendances.update-status');
+    // 📋 Routes accessible by all logged-in users
+    // Attendances
+    Route::get('/attendances', [AttendanceController::class, 'index'])->name('attendances.index');
+    Route::get('/attendances/create', [AttendanceController::class, 'create'])->name('attendances.create');
+    Route::get('/attendances/creates', [AttendanceController::class, 'creates'])->name('attendances.creates');
+    Route::post('/attendances', [AttendanceController::class, 'store'])->name('attendances.store');
+    Route::delete('/attendances/{id}', [AttendanceController::class, 'destroy'])->name('attendances.destroy');
+    Route::get('/employees-attendances', [AttendanceController::class, 'indexs'])->name('employees_attendances.index');
+    Route::patch('/attendances/{id}/status', [AttendanceController::class, 'updateStatus'])->name('attendances.update-status');
 
     // Complaints
     Route::resource('complaints', ComplaintController::class);
     Route::get('/get-employee/{location}', [ComplaintController::class, 'getEmployeeByLocation']);
     Route::get('/get-employee-by-location/{locationId}', [ComplaintController::class, 'getEmployeeByLocation']);
+
+    Route::prefix('workreports')->controller(WorkReportController::class)->group(function () {
+        Route::get('/', 'index')->name('workreports.index');
+        Route::get('/create', 'create')->name('workreports.create');
+        Route::post('/', 'store')->name('workreports.store');
+        Route::get('/{id}/edit', 'edit')->name('workreports.edit');
+        Route::put('/{id}', 'update')->name('workreports.update');
+        Route::delete('/{id}', 'destroy')->name('workreports.destroy');
+    });
+
+    Route::get('/lostitems', [LostItemController::class, 'index'])->name('lostitem.index');
+    Route::get('/lostitems/create', [LostItemController::class, 'create'])->name('lostitem.create');
+    Route::post('/lostitems', [LostItemController::class, 'store'])->name('lostitem.store');
+    Route::get('/lostitems/{id}/edit', [LostItemController::class, 'edit'])->name('lostitem.edit');
+    Route::put('/lostitems/{id}', [LostItemController::class, 'update'])->name('lostitem.update');
+    Route::delete('/lostitems/{id}', [LostItemController::class, 'destroy'])->name('lostitem.destroy');
+
+    Route::get('/itemfounds/create', [ItemFoundController::class, 'create'])->name('itemfound.create');
+    Route::post('/itemfounds', [ItemFoundController::class, 'store'])->name('itemfound.store');
+    Route::get('/itemfounds/{id}/edit', [ItemFoundController::class, 'edit'])->name('itemfound.edit');
+    Route::put('/itemfounds/{id}', [ItemFoundController::class, 'update'])->name('itemfound.update');
+    Route::delete('/itemfounds/{id}', [ItemFoundController::class, 'destroy'])->name('itemfound.destroy');
+
+    Route::resource('employees', controller: EmployeeController::class);
+});
